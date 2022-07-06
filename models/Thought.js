1aa116fec0f0f02be4e0fde8cad66da845bf5df8
Rafael Herrera <rafaelherrera731@gmail.com>
2022-07-05 20:18:12 -0700

    editing thought schema to match requirements

--- models/Thought.js
+++ models/Thought.js
@@ -4,23 +4,22 @@ const Response = require('./Response');
 // Schema to create Post model
 const thoughtSchema = new Schema(
   {
-    published: {
-      type: Boolean,
-      default: false,
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxlenght: 280,
     },
     createdAt: {
       type: Date,
       default: Date.now,
     },
-    advertiserFriendly: {
-      type: Boolean,
-      default: true,
-    },
-    description: {
+    
+    username: {
       type: String,
-      minLength: 15,
-      maxLength: 500,
+      required: true,
     },
+    
     responses: [Response],
   },
   {
